56575e3513a5166cfdd6467e0964ccf172ffb06d
kalinswartz <kalinswartz@gmail.com>
2024-09-07 16:54:20 -0500

    new user books association - book collection 4 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   
-  root "books#index"
+  root "user_books#index"
 
   resources :books do 
     member do
       get :delete
     end
   end
-
-  #get 'books/index'
-  #get 'books/new'
-  #get 'books/edit'
-  #get 'books/show'
-  #get 'books/delete'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
